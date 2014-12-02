736d2eeea884da33bd0612ef0e939a72e0b7c13d
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-12-02 17:52:02 +0100

    Adjust the database model
    With this code the database can be created simply by calling create_tables
    and all that's required in sqlalchemy

--- fresque/lib/models.py
+++ fresque/lib/models.py
@@ -1,85 +1,222 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import absolute_import, unicode_literals, print_function
 
-from flask import current_app
-from sqlalchemy import func
+import sqlalchemy as sa
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import relation
+from sqlalchemy.orm import backref
 
-from fresque.lib import states
 
-db = current_app.extensions["sqlalchemy"].db
+BASE = declarative_base()
 
-#class User(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    nickname = db.Column(db.String(64), index=True, unique=True)
-#    email = db.Column(db.String(120), index=True, unique=True)
+
+def create_tables(db_url, alembic_ini=None, debug=False):
+    """ Create the tables in the database using the information from the
+    url obtained.
+
+    :arg db_url, URL used to connect to the database. The URL contains
+        information with regards to the database engine, the host to
+        connect to, the user and password and the database name.
+          ie: <engine>://<user>:<password>@<host>/<dbname>
+    :kwarg alembic_ini, path to the alembic ini file. This is necessary
+        to be able to use alembic correctly, but not for the unit-tests.
+    :kwarg debug, a boolean specifying wether we should have the verbose
+        output of sqlalchemy or not.
+    :return a session that can be used to query the database.
+
+    """
+    engine = sa.create_engine(db_url, echo=debug)
+    BASE.metadata.create_all(engine)
+    #engine.execute(collection_package_create_view(driver=engine.driver))
+    if db_url.startswith('sqlite:'):
+        ## Ignore the warning about con_record
+        # pylint: disable=W0613
+        def _fk_pragma_on_connect(dbapi_con, con_record):
+            ''' Tries to enforce referential constraints on sqlite. '''
+            dbapi_con.execute('pragma foreign_keys=ON')
+        sa.event.listen(engine, 'connect', _fk_pragma_on_connect)
+
+    if alembic_ini is not None:  # pragma: no cover
+        # then, load the Alembic configuration and generate the
+        # version table, "stamping" it with the most recent rev:
+
+        ## Ignore the warning missing alembic
+        # pylint: disable=F0401
+        from alembic.config import Config
+        from alembic import command
+        alembic_cfg = Config(alembic_ini)
+        command.stamp(alembic_cfg, "head")
+
+    scopedsession = scoped_session(sessionmaker(bind=engine))
+    return scopedsession
+
+
+#class User(sa.Model):
+#    id = sa.Column(sa.Integer, primary_key=True)
+#    nickname = sa.Column(sa.String(64), index=True, unique=True)
+#    email = sa.Column(sa.String(120), index=True, unique=True)
 #
 
 
-class Package(db.Model):
-    id      = db.Column(db.Integer, primary_key=True)
-    name    = db.Column(db.String(128), nullable=False, index=True, unique=True)
-    summary = db.Column(db.String(255), nullable=False)
-    owner   = db.Column(db.String(255), db.ForeignKey("user.name", onupdate="cascade"))
+class Package(BASE):
+    __tablename__ = 'packages'
+
+    id = sa.Column(
+        sa.Integer,
+        primary_key=True)
+    name = sa.Column(
+        sa.String(128),
+        nullable=False,
+        index=True,
+        unique=True)
+    summary = sa.Column(
+        sa.String(255),
+        nullable=False)
+    owner = sa.Column(
+        sa.String(255),
+        sa.ForeignKey(
+            "users.name",
+            onupdate="cascade")
+        )
     # The state could use an Enum type, but we don't need the space-savings and
     # strict model checks that come with the added complexity in migrations.
-    state   = db.Column(db.String(64), nullable=False, default="new", index=True)
+    state = sa.Column(
+        sa.String(64),
+        nullable=False,
+        default="new",
+        index=True)
 
     def __repr__(self):
         return '<Package %r>' % (self.name)
 
 
-class Distribution(db.Model):
-    id = db.Column(db.String(32), primary_key=True)
-    name = db.Column(db.String(64), unique=True)
+class Distribution(BASE):
+    __tablename__ = 'distributions'
+
+    id = sa.Column(
+        sa.String(32),
+        primary_key=True)
+    name = sa.Column(
+        sa.String(64),
+        unique=True)
 
     def __repr__(self):
         return '<Distribution %r>' % (self.id)
 
 
-target_distributions = db.Table('target_distributions',
-    db.Column('package_id', db.Integer,
-        db.ForeignKey('package.id'), primary_key=True),
-    db.Column('distribution_id', db.String(32),
-        db.ForeignKey('distribution.id'), primary_key=True)
-)
+class TargetDistribution(BASE):
+    __tablename__ = 'target_distributions'
+
+    package_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('packages.id'),
+        primary_key=True),
+    distribution_id = sa.Column(
+        sa.String(32),
+        sa.ForeignKey('distributions.id'),
+        primary_key=True)
 
 
-class User(db.Model):
+class User(BASE):
+    __tablename__ = 'users'
     # From FAS
-    name     = db.Column(db.String(255), primary_key=True)
-    fullname = db.Column(db.String(255))
-    email    = db.Column(db.String(255))
+    name = sa.Column(
+        sa.String(255),
+        primary_key=True)
+    fullname = sa.Column(
+        sa.String(255))
+    email = sa.Column(
+        sa.String(255))
 
     def __repr__(self):
         return '<User %r>' % (self.name)
 
 
-class Review(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    package_id = db.Column(db.Integer, db.ForeignKey("package.id", ondelete="cascade", onupdate="cascade"))
-    commit_id  = db.Column(db.String(128), index=True, nullable=False)
-    date_start = db.Column(db.DateTime, index=True, nullable=False, default=db.func.now())
-    date_end   = db.Column(db.DateTime, index=True)
-    srpm_filename = db.Column(db.String(255))
-    spec_filename = db.Column(db.String(255))
-    active     = db.Column(db.Boolean, nullable=False, default=True)
+class Review(BASE):
+    __tablename__ = 'reviews'
+
+    id = sa.Column(
+        sa.Integer,
+        primary_key=True)
+    package_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey(
+            "packages.id",
+            ondelete="cascade",
+            onupdate="cascade")
+        )
+    commit_id  = sa.Column(
+        sa.String(128),
+        index=True,
+        nullable=False)
+    date_start = sa.Column(
+        sa.DateTime,
+        index=True,
+        nullable=False,
+        default=sa.func.now())
+    date_end = sa.Column(
+        sa.DateTime,
+        index=True)
+    srpm_filename = sa.Column(
+        sa.String(255))
+    spec_filename = sa.Column(
+        sa.String(255))
+    active = sa.Column(
+        sa.Boolean,
+        nullable=False,
+        default=True)
 
     def __repr__(self):
         return '<Review %r of package %r>' % (self.id, self.package_id)
 
 
-reviewers = db.Table("reviewers",
-    db.Column('review_id', db.Integer, db.ForeignKey('package.id')),
-    db.Column('reviewer_name', db.String(255), db.ForeignKey('user.name'))
-)
+class Reviewer(BASE):
+    __tablename__ = 'reviewers'
+
+    review_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('packages.id'),
+        primary_key=True)
+    reviewer_name = sa.Column(
+        sa.String(255),
+        sa.ForeignKey('users.name'),
+        primary_key=True)
 
 
-class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    review_id    = db.Column(db.Integer, db.ForeignKey("review.id", ondelete="cascade", onupdate="cascade"))
-    author       = db.Column(db.String(255), db.ForeignKey("user.name", onupdate="cascade"))
-    date         = db.Column(db.DateTime, nullable=False, default=db.func.now())
-    line_number  = db.Column(db.Integer)
-    relevant     = db.Column(db.Boolean, nullable=False, default=True) # Is the comment still relevant
+class Comment(BASE):
+    __tablename__ = 'comments'
+
+    id = sa.Column(
+        sa.Integer,
+        primary_key=True)
+    review_id = sa.Column(
+        sa.Integer,
+        sa.ForeignKey(
+            "reviews.id",
+            ondelete="cascade",
+            onupdate="cascade")
+        )
+    author = sa.Column(
+        sa.String(255),
+        sa.ForeignKey(
+            "users.name",
+            onupdate="cascade")
+        )
+    date = sa.Column(
+        sa.DateTime,
+        nullable=False,
+        default=sa.func.now())
+    line_number = sa.Column(
+        sa.Integer)
+    relevant = sa.Column(
+        sa.Boolean,
+        nullable=False,
+        default=True) # Is the comment still relevant
 
     def __repr__(self):
         return '<Comment %r on %r>' % (self.id, review_id)
