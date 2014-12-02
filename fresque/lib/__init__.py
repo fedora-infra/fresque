c879486d71328e9758084d53a9129d3f4f5421f9
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-12-02 17:20:11 +0100

    Add an __init__ file in the lib module to make it a full python module

---
+++ fresque/lib/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+'''
+Internal library for the fresque application.
+
+This module and all its files contains all the operations independant of the
+framework and should be completely covered in unit-tests.
+'''
+
+import sqlalchemy as sa
+import fresque.lib.database as db
+
