74f3f70337e9924e4fce030d6a5941ce506bfee9
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-12-02 18:01:33 +0100

    Add a runserver script to start the application for development purposes

---
+++ runserver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+## These two lines are needed to run on EL6
+__requires__ = ['SQLAlchemy >= 0.8', 'jinja2 >= 2.4']
+import pkg_resources
+
+import sys
+from werkzeug.contrib.profiler import ProfilerMiddleware
+
+from fresque import APP
+APP.debug = True
+
+if '--profile' in sys.argv:
+    APP.config['PROFILE'] = True
+    APP.wsgi_app = ProfilerMiddleware(APP.wsgi_app, restrictions=[30])
+
+APP.run()
