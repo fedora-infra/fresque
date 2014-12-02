2379f5943a0d8d4f5243a0ac0bd895f4673c900a
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-12-02 17:54:12 +0100

    Small changes to the views fixing imports and adding some documentation

--- fresque/views.py
+++ fresque/views.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Views fro the fresque flask application
+'''
+
 from __future__ import absolute_import, unicode_literals, print_function
 
-from fresque import app
+from fresque import APP
 
 
-@app.route("/")
+@APP.route("/")
 def index():
     pass
