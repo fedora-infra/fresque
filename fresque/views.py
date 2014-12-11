384849b6a49e0169ccc1f5630caf0e65a2e1599a
Aurélien Bompard <aurelien@bompard.org>
2014-12-11 16:12:52 +0100

    Add basic HTML templates (and some mockups)

--- fresque/views.py
+++ fresque/views.py
@@ -6,9 +6,39 @@ Views for the fresque flask application
 
 from __future__ import absolute_import, unicode_literals, print_function
 
+from flask import request, g, render_template
+
 from fresque import APP
 
 
 @APP.route("/")
 def index():
-    pass
+    return render_template("index.html")
+
+@APP.route("/search")
+def search():
+    raise NotImplementedError
+
+@APP.route("/new")
+def newpackage():
+    return "newpackage"
+
+@APP.route("/my/packages")
+def user_packages():
+    return "user_packages"
+
+@APP.route("/my/reviews")
+def user_reviews():
+    return "user_reviews"
+
+@APP.route("/review/<int:rid>")
+def review(rid):
+    return "review %d" % rid
+
+@APP.route("/login")
+def login():
+    return "login"
+
+@APP.route("/logout")
+def logout():
+    return "logout"
