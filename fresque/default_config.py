61d693ef095208d83f8a659b56acc45e11c19959
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-12-15 13:16:22 +0100

    Document configuration and set a time out for the session

--- fresque/default_config.py
+++ fresque/default_config.py
@@ -2,13 +2,25 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
-basedir = os.path.abspath(os.path.dirname(__file__)) # pylint: disable=invalid-name
 
+from datetime import timedelta
+
+basedir = os.path.abspath(
+    os.path.dirname(__file__)) # pylint: disable=invalid-name
+
+# Set the time after which the session expires. Flask's default is 31 days.
+# Default: ``timedelta(days=1)`` corresponds to 1 day.
+PERMANENT_SESSION_LIFETIME = timedelta(days=1)
+
+
+# url to the database server:
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
     basedir, '..', 'fresque.sqlite')
-# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
+
+# secret key used to generate unique csrf token
 SECRET_KEY = 'Change-me-Im-famous'
+
 
 DISTRIBUTIONS = {
     "f21": "Fedora 21",
