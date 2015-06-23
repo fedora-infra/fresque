635945eaa02bf39e47be52a1e4f9e800d5340dba
Rahul Ranjan <rahul.rrixe@gmail.com>
2015-06-23 16:16:56 +0530

    add jinja filters

--- fresque/__init__.py
+++ fresque/__init__.py
@@ -18,21 +18,9 @@ APP = flask.Flask(__name__)
 APP.jinja_env.trim_blocks = True
 APP.jinja_env.lstrip_blocks = True
 
-# # add jinja filters
-# filter_name = {
-#     'shorted_commit': 'short',
-#     'human_readable_time': 'humanize'
-# }
-# # update jinja filter with flask app env
-# jinja_filters = {filter_name[name]: function
-#                     for name, function in getmembers(filters)
-#                     if isfunction(function)}
-
-# APP.jinja_env.filters.update(jinja_filters)
-
-# APP.config.from_object('fresque.default_config')
-# if 'FRESQUE_CONFIG' in os.environ: # pragma: no cover
-#     APP.config.from_envvar('FRESQUE_CONFIG')
+APP.config.from_object('fresque.default_config')
+if 'FRESQUE_CONFIG' in os.environ: # pragma: no cover
+     APP.config.from_envvar('FRESQUE_CONFIG')
 
 # Set up FAS extension
 FAS = flask_fas_openid.FAS(APP)
