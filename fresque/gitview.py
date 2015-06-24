30e6c061a5d2f83cc7a1b0ea744d2295b44f2fe6
Rahul Ranjan <rahul.rrixe@gmail.com>
2015-06-25 02:19:40 +0530

    add link to the git web in package view file

--- fresque/gitview.py
+++ fresque/gitview.py
@@ -11,6 +11,16 @@ import flask
 import pygit2
 import fresque
 
+from sqlalchemy.orm import aliased
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import guess_lexer_for_filename
+from pygments.lexers.special import TextLexer
+from pygments.util import ClassNotFound
+
+import mimetypes
+import chardet
+
 from fresque import doc_utils
 from fresque import APP
 from fresque.lib.git import Repository
@@ -26,44 +36,145 @@ def get_repo_by_name(name):
     return repo_obj
 
 
-@APP.route("/git/repo/<name>")
-def repo_base_view(name):
-    repo_obj = get_repo_by_name(name)
+def __get_file_in_tree(repo_obj, tree, filepath):
+    ''' Retrieve the entry corresponding to the provided filename in a
+    given tree.
+    '''
+    filename = filepath[0]
+    if isinstance(tree, pygit2.Blob):
+        return
+    for entry in tree:
+        if entry.name == filename:
+            if len(filepath) == 1:
+                return repo_obj[entry.oid]
+            else:
+                return __get_file_in_tree(
+                    repo_obj, repo_obj[entry.oid], filepath[1:])
 
-    cnt = 0
-    last_commits = []
+
+@APP.route("/git/repo/<packagename>")
+def repo_base_view(packagename):
+    repo_obj = get_repo_by_name(packagename)
+
     tree = []
     if not repo_obj.is_empty:
         try:
-            for commit in repo_obj.walk(
-                    repo_obj.head.target, pygit2.GIT_SORT_TIME):
-                last_commits.append(commit)
-                cnt += 1
-                if cnt == 3:
-                    break
-            tree = sorted(last_commits[0].tree, key=lambda x: x.filemode)
+            last_commit = repo_obj.get_last_commit()
+            tree = sorted(last_commit.tree, key=lambda x: x.filemode)
         except pygit2.GitError:
             pass
-    # show readme
-    readme = None
-    safe = False
-    for i in tree:
-        name, ext = os.path.splitext(i.name)
-        if name == 'README':
-            content = repo_obj[i.oid].data
-            readme, safe = doc_utils.convert_readme(
-                content, ext,
-                view_file_url="#")
 
     return flask.render_template(
         '/git/repo.html',
         select="files",
         tree=tree,
-        last_commit=repo_obj.get_last_commit(),
-        repo=name,
+        last_commit=last_commit,
+        repo=packagename,
         repo_obj=repo_obj,
-        username=flask.g.fas_user.username,
-        readme=readme,
         branches=sorted(repo_obj.listall_branches()),
         branchname='master'
     )
+
+
+@APP.route('/git/<repo>/blob/<path:identifier>/f/<path:filename>')
+def view_file(repo, identifier, filename):
+    """ Displays the content of a file or a tree for the specified repo.
+    """
+    if not repo:
+        flask.abort(404, 'Project not found')
+
+    repo_obj = get_repo_by_name(repo)
+
+    if repo_obj.is_empty:
+        flask.abort(404, 'Empty repo cannot have a file')
+
+    if identifier in repo_obj.listall_branches():
+        branchname = identifier
+        branch = repo_obj.lookup_branch(identifier)
+        commit = branch.get_object()
+    else:
+        try:
+            commit = repo_obj.get(identifier)
+            branchname = identifier
+        except ValueError:
+            if 'master' not in repo_obj.listall_branches():
+                flask.abort(404, 'Branch no found')
+            # If it's not a commit id then it's part of the filename
+            commit = repo_obj[repo_obj.head.target]
+            branchname = 'master'
+
+    if commit and not isinstance(commit, pygit2.Blob):
+        content = __get_file_in_tree(
+            repo_obj, commit.tree, filename.split('/'))
+        if not content:
+            flask.abort(404, 'File not found')
+        content = repo_obj[content.oid]
+    else:
+        content = commit
+
+    if isinstance(content, pygit2.Blob):
+        if content.is_binary:
+            output_type = 'binary'
+        else:
+            try:
+                lexer = guess_lexer_for_filename(
+                    filename,
+                    content.data
+                )
+            except ClassNotFound:
+                lexer = TextLexer()
+
+            content = highlight(
+                content.data,
+                lexer,
+                HtmlFormatter(
+                    noclasses=True,
+                    style="tango",)
+            )
+            output_type = 'file'
+    else:
+        content = sorted(content, key=lambda x: x.filemode)
+        output_type = 'tree'
+
+    return flask.render_template(
+        '/git/file.html',
+        select='files',
+        repo=repo,
+        branchname=branchname,
+        filename=filename,
+        content=content,
+        output_type=output_type,
+        last_commit=repo_obj.get_last_commit()
+    )
+
+
+@APP.route('/git/<repo>/raw/<identifier>/<filename>')
+def view_raw_file(repo, identifier, filename):
+    refpath = os.path.join(identifier, filename)
+
+    repo_obj = get_repo_by_name(repo)
+
+    ref, path, tree, file = repo_obj.split_ref(
+        repo, refpath, expects_file=True)
+
+    if not ref:
+        raise IOError("No such file")
+
+    # Try to detect the mimetype
+    mimetype, encoding = mimetypes.guess_type(file.name)
+    data = repo[file.hex].data
+    # shbang'ed: text/plain will do
+    if not mimetype and data[:2] == '#!':
+        mimetype = 'text/plain'
+    # For text mimetypes, guess an encoding
+    if not mimetype:
+        if '\0' in data:
+            mimetype = 'application/octet-stream'
+        else:
+            mimetype = 'text/plain'
+    if mimetype.startswith('text/') and not encoding:
+        encoding = chardet.detect(data)['encoding']
+    headers = {'Content-Type': mimetype}
+    if encoding:
+        headers['Content-Encoding'] = encoding
+    return (data, 200, headers)
