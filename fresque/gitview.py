9f6a92510e6d990b12031b76fda9aaf87ab209e1
Rahul Ranjan <rahul.rrixe@gmail.com>
2015-06-25 18:12:59 +0530

    remove split_ref and simplify view_raw_file controller

--- fresque/gitview.py
+++ fresque/gitview.py
@@ -11,6 +11,8 @@ import flask
 import pygit2
 import fresque
 
+import kitchen.text.converters as ktc
+
 from sqlalchemy.orm import aliased
 from pygments import highlight
 from pygments.formatters import HtmlFormatter
@@ -21,7 +23,6 @@ from pygments.util import ClassNotFound
 import mimetypes
 import chardet
 
-from fresque import doc_utils
 from fresque import APP
 from fresque.lib.git import Repository
 
@@ -150,30 +151,71 @@ def view_file(repo, identifier, filename):
 
 @APP.route('/git/<repo>/raw/<path:identifier>/f/<path:filename>')
 def view_raw_file(repo, identifier, filename):
-    refpath = os.path.join(identifier, filename)
+
+    if not repo:
+        flask.abort(404, 'Project not found')
 
     repo_obj = get_repo_by_name(repo)
 
-    ref, path, tree, file = repo_obj.split_ref(refpath, expects_file=True)
+    if repo_obj.is_empty:
+        flask.abort(404, 'Empty repo cannot have a file')
 
-    if not ref:
-        raise IOError("No such file")
+    if identifier in repo_obj.listall_branches():
+        branch = repo_obj.lookup_branch(identifier)
+        commit = branch.get_object()
+    else:
+        try:
+            commit = repo_obj.get(identifier)
+        except ValueError:
+            if 'master' not in repo_obj.listall_branches():
+                flask.abort(404, 'Branch no found')
+            # If it's not a commit id then it's part of the filename
+            commit = repo_obj[repo_obj.head.target]
 
-    # Try to detect the mimetype
-    mimetype, encoding = mimetypes.guess_type(file.name)
-    data = repo_obj[file.hex].data
-    # shbang'ed: text/plain will do
+    if not commit:
+        flask.abort(400, 'Commit %s not found' % (identifier))
+
+    mimetype = None
+    encoding = None
+    if filename:
+        content = __get_file_in_tree(
+            repo_obj, commit.tree, filename.split('/'))
+        if not content or isinstance(content, pygit2.Tree):
+            flask.abort(404, 'File not found')
+
+        mimetype, encoding = mimetypes.guess_type(filename)
+        data = repo_obj[content.oid].data
+    else:
+        if commit.parents:
+            diff = commit.tree.diff_to_tree()
+
+            try:
+                parent = repo_obj.revparse_single('%s^' % identifier)
+                diff = repo_obj.diff(parent, commit)
+            except (KeyError, ValueError):
+                flask.abort(404, 'Identifier not found')
+        else:
+            # First commit in the repo
+            diff = commit.tree.diff_to_tree(swap=True)
+        data = diff.patch
+
+    if not data:
+        flask.abort(404, 'No content found')
+
     if not mimetype and data[:2] == '#!':
         mimetype = 'text/plain'
-    # For text mimetypes, guess an encoding
+
     if not mimetype:
         if '\0' in data:
             mimetype = 'application/octet-stream'
         else:
             mimetype = 'text/plain'
+
     if mimetype.startswith('text/') and not encoding:
-        encoding = chardet.detect(data)['encoding']
+        encoding = chardet.detect(ktc.to_bytes(data))['encoding']
+
     headers = {'Content-Type': mimetype}
     if encoding:
         headers['Content-Encoding'] = encoding
+
     return (data, 200, headers)
